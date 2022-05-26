b33a0150a4ecb70a0fa0af5e8909e4ba480399a4
juanma5822 <juanma5822@gmail.com>
2022-05-25 20:49:11 -0500

    Rutas agregadas

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, redirect, url_for, request, jsonify
-from flask_mysqldb import MySQL
+from flask import Flask, redirect, url_for, request, json
+import psycopg2
 app = Flask(__name__)   
-app.config['MYSQL_HOST'] = 'ec2-34-227-120-79.compute-1.amazonaws.com'
-app.config['MYSQL_USER'] = 'xxbafeigeplqzf'
-app.config['MYSQL_PASSWORD'] = '292103424cc1246aebe3a724ba93b366f7bcb06ae9d2c700b1cfd305b03e6d80'
-app.config['MYSQL_DB'] = 'd4reu6rdlo91uc'
-app.config['MYSQL_PORT']= 5432
-mysql = MySQL(app)
+conexion=psycopg2.connect(user='xxbafeigeplqzf',
+                          password='292103424cc1246aebe3a724ba93b366f7bcb06ae9d2c700b1cfd305b03e6d80',
+                          host='ec2-34-227-120-79.compute-1.amazonaws.com',
+                          port=5432,
+                          database='d4reu6rdlo91uc')
+
+cursor=conexion.cursor()
 
 @app.route("/")
 def Home():
@@ -15,11 +16,12 @@ def Home():
 
 @app.route("/getData")
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM d4reu6rdlo91uc ")
-    data = cur.fetchall()
-    cur.close()
-    return(data)
+    cursor
+    sql=("SELECT * FROM datosrecibidos ")
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    response = data
+    return(json.dumps(response))
 
 if __name__ == '__main__':
     app.run(port=5432)

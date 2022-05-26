93d49e82c897f15dbbab7229174bb1baf0c944a5
juanma5822 <juanma5822@gmail.com>
2022-05-25 19:17:31 -0500

    Version 1.0 con cambios de data

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, request, jsonify
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'ec2-34-227-120-79.compute-1.amazonaws.com'
-app.config['MYSQL_USER'] = 'mlsrxudbzbqxlx'
-app.config['MYSQL_PASSWORD'] = 'b25c9ec282a5af40a9a1b1b790616a8c94b6c446fce3a0f5604225251c2cd621'
-app.config['MYSQL_DB'] = 'd9s8mci0uikpqd'
+app.config['MYSQL_USER'] = 'xxbafeigeplqzf'
+app.config['MYSQL_PASSWORD'] = '292103424cc1246aebe3a724ba93b366f7bcb06ae9d2c700b1cfd305b03e6d80'
+app.config['MYSQL_DB'] = 'd4reu6rdlo91uc'
 app.config['MYSQL_PORT']= 5432
 mysql = MySQL(app)
 
